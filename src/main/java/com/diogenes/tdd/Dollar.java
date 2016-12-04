7a0cfdb69c57303aeffcbc133bd1bb78685e0cfe
diogenesaraujo <diogenes.w.araujo@gmail.com>
2016-12-04 20:17:46 -0300

    TDD second step - Solved compilation problemns. In fact, which correction was a step

---
+++ src/main/java/com/diogenes/tdd/Dollar.java
@@ -0,0 +1,14 @@
+package com.diogenes.tdd;
+
+/**
+ * Created by daraujo on 04/12/16.
+ */
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+    }
+
+    public void times(int multiplier) {
+    }
+}
--- src/test/java/com/diogenes/tdd/DollarTest.java
+++ src/test/java/com/diogenes/tdd/DollarTest.java
@@ -1,9 +1,12 @@
 package com.diogenes.tdd;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by daraujo on 04/12/16.
  */
 public class DollarTest {
+
     @org.junit.Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
