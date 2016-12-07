2a0bfdec14df473b199d6ef0cd56505e43b04664
diogenesaraujo <diogenes.w.araujo@gmail.com>
2016-12-07 11:22:10 -0300

    Refactor step - Eliminate duplication

--- src/main/java/com/diogenes/tdd/Dollar.java
+++ src/main/java/com/diogenes/tdd/Dollar.java
@@ -4,11 +4,13 @@ package com.diogenes.tdd;
  * Created by daraujo on 04/12/16.
  */
 public class Dollar {
-    public int amount = 10;
+    public int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
+        amount *= multiplier;
     }
 }
