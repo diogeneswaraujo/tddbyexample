4ce9dcbbb7cb745a0adee120ef927f8e16ff1620
diogenesaraujo <diogenes.w.araujo@gmail.com>
2016-12-08 11:03:48 -0300

    I couldnt imagine a clean way to get the previous test to work, thats why we change the Dollar interface and code broke

--- src/test/java/com/diogenes/tdd/DollarTest.java
+++ src/test/java/com/diogenes/tdd/DollarTest.java
@@ -10,9 +10,11 @@ public class DollarTest {
     @org.junit.Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-        five.times(3);
-        assertEquals(15, five.amount);
+        Dollar product = five.times(2);
+
+        assertEquals(10, product.amount);
+
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
