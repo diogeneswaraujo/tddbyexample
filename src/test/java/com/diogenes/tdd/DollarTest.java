242dcf4fe66261c9c101b23f66f3d3e896c222a1
diogenesaraujo <diogenes.w.araujo@gmail.com>
2016-12-08 11:00:22 -0300

    test fail

--- src/test/java/com/diogenes/tdd/DollarTest.java
+++ src/test/java/com/diogenes/tdd/DollarTest.java
@@ -12,5 +12,7 @@ public class DollarTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 }
