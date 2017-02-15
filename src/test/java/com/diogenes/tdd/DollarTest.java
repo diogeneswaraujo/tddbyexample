a7cf3ed5e43a3b7a45b61a7ab2268fa0fd371c27
Diogenes Araujo <diogenes.w.araujo@gmail.com>
2017-02-15 09:32:56 -0200

    Franc equals Dollar?

--- src/test/java/com/diogenes/tdd/DollarTest.java
+++ src/test/java/com/diogenes/tdd/DollarTest.java
@@ -34,5 +34,7 @@ public class DollarTest {
 
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 }
